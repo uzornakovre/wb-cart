a96e5cb150227258b8ab326408a2d3b12ae67501
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-11-02 20:49:04 +0300

    refactor: replace cart items to template

---
+++ src/components/cart.js
@@ -0,0 +1,28 @@
+export class Cart {
+  constructor({ renderer }, containerSelector) {
+    this._renderer = renderer;
+    this._container = containerSelector;
+  }
+
+  renderItems(productItems) {
+    productItems.forEach((item) => {
+      if (item.count) {
+        this._item = this._renderer(item);
+        this._addItem(this._item);
+      }
+    });
+  }
+
+  renderOutOfStockItems(productItems) {
+    productItems.forEach((item) => {
+      if (!item.count) {
+        this._item = this._renderer(item);
+        this._addItem(this._item);
+      }
+    });
+  }
+
+  _addItem(card) {
+    this._container.append(card);
+  }
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,15 +1,20 @@
-const PRODUCTS_LIST = [
+import tShirtImg from "../images/item_1.jpg";
+import caseImg from "../images/item_2.jpg";
+import pencilsImg from "../images/item_3.jpg";
+
+export const PRODUCTS_LIST = [
   {
-    id: 1,
+    id: "prod-1",
     name: "Футболка UZcotton мужская",
+    image: tShirtImg,
     properties: {
       color: "белый",
-      size: 56,
+      size: "56",
     },
     store: "Коледино WB",
     seller: {
-      name: "ООО Вайлдберрис",
-      fullName: "OOO «Вайлдберрис»",
+      name: "ООО Вайлдберриз",
+      fullName: "OOO «Вайлдберриз»",
       registerNumber: 5167746237148,
       address:
         "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
@@ -19,30 +24,32 @@ const PRODUCTS_LIST = [
     count: 2,
   },
   {
-    id: 2,
+    id: "prod-2",
     name: "Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe",
+    image: caseImg,
     properties: {
       color: "прозрачный",
     },
     store: "Коледино WB",
     seller: {
-      name: "ООО Мегапростиль",
+      name: "ООО Мегапрофстиль",
       fullName: "OOO «МЕГАПРОФСТИЛЬ»",
       registerNumber: 5167746237148,
       address:
         "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
     },
-    price: 15000,
+    price: 15000000,
     discount: 55,
     count: 300,
   },
   {
-    id: 3,
+    id: "prod-3",
     name: 'Карандаши цветные Faber\u2011Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber\u2011Castell',
-    properties: {},
+    image: pencilsImg,
+    properties: null,
     store: "Коледино WB",
     seller: {
-      name: "ООО Вайлдберрис",
+      name: "ООО Вайлдберриз",
       fullName: "OOO «МЕГАПРОФСТИЛЬ»",
       registerNumber: 5167746237148,
       address:
@@ -52,4 +59,62 @@ const PRODUCTS_LIST = [
     discount: 20,
     count: 2,
   },
+  {
+    id: "prod-4",
+    name: "Футболка UZcotton мужская",
+    image: tShirtImg,
+    properties: {
+      color: "белый",
+      size: "56",
+    },
+    store: "Коледино WB",
+    seller: {
+      name: "ООО Вайлдберриз",
+      fullName: "OOO «Вайлдберриз»",
+      registerNumber: 5167746237148,
+      address:
+        "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
+    },
+    price: 1305,
+    discount: 50,
+    count: 0,
+  },
+  {
+    id: "prod-5",
+    name: "Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe",
+    image: caseImg,
+    properties: {
+      color: "прозрачный",
+    },
+    store: "Коледино WB",
+    seller: {
+      name: "ООО Мегапрофстиль",
+      fullName: "OOO «МЕГАПРОФСТИЛЬ»",
+      registerNumber: 5167746237148,
+      address:
+        "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
+    },
+    price: 15000000,
+    discount: 55,
+    count: 0,
+  },
+  {
+    id: "prod-6",
+    name: 'Карандаши цветные Faber\u2011Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber\u2011Castell',
+    image: pencilsImg,
+    properties: null,
+    store: "Коледино WB",
+    seller: {
+      name: "ООО Вайлдберриз",
+      fullName: "OOO «МЕГАПРОФСТИЛЬ»",
+      registerNumber: 5167746237148,
+      address:
+        "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
+    },
+    price: 950,
+    discount: 20,
+    count: 0,
+  },
 ];
+
+export const USER_DISCOUNT = 10;
