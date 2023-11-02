b6d4f4c8feba78e44883d02a5bb2c3ee9905d93c
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-11-02 14:36:56 +0300

    feat: add products list

---
+++ src/utils/constants.js
@@ -0,0 +1,55 @@
+const PRODUCTS_LIST = [
+  {
+    id: 1,
+    name: "Футболка UZcotton мужская",
+    properties: {
+      color: "белый",
+      size: 56,
+    },
+    store: "Коледино WB",
+    seller: {
+      name: "ООО Вайлдберрис",
+      fullName: "OOO «Вайлдберрис»",
+      registerNumber: 5167746237148,
+      address:
+        "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
+    },
+    price: 1305,
+    discount: 50,
+    count: 2,
+  },
+  {
+    id: 2,
+    name: "Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe",
+    properties: {
+      color: "прозрачный",
+    },
+    store: "Коледино WB",
+    seller: {
+      name: "ООО Мегапростиль",
+      fullName: "OOO «МЕГАПРОФСТИЛЬ»",
+      registerNumber: 5167746237148,
+      address:
+        "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
+    },
+    price: 15000,
+    discount: 55,
+    count: 300,
+  },
+  {
+    id: 3,
+    name: 'Карандаши цветные Faber\u2011Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber\u2011Castell',
+    properties: {},
+    store: "Коледино WB",
+    seller: {
+      name: "ООО Вайлдберрис",
+      fullName: "OOO «МЕГАПРОФСТИЛЬ»",
+      registerNumber: 5167746237148,
+      address:
+        "129337, Москва, улица Красная Сосна, 2, корпус 1, стр. 1, помещение 2, офис 34",
+    },
+    price: 950,
+    discount: 20,
+    count: 2,
+  },
+];
