9225b2b00eb459094e4c6874811e89c47a705eb1
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-11-06 06:49:00 +0300

    refactor: render payment-method items by template

---
+++ src/components/modal.js
@@ -0,0 +1,36 @@
+export default class Modal {
+  constructor(modal) {
+    this._modal = modal;
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._modal.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  close() {
+    this._modal.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._modal.addEventListener("mousedown", (evt) => {
+      if (
+        evt.target.classList.contains("modal_opened") ||
+        evt.target.classList.contains("modal__close-button")
+      ) {
+        this.close();
+      }
+    });
+    this._modal
+      .querySelector(".modal__close-button")
+      .addEventListener("click", this.close.bind(this));
+  }
+}
